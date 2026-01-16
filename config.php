a01092051e0f34be4ebce46e8299d5b85f6a83b6
SQLIGROUP\aginepro <aginepro@sqli.com>
2026-01-16 17:22:26 +0100

    config updated

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 define('DB_SERVER', '192.168.10.18');
 define('DB_USERNAME', 'db_connect'); 
-define('DB_PASSWORD', '1234');  
+define('DB_PASSWORD', 'W@tchD0g_S3cur1ty!2026');  
 define('DB_NAME', 'detechtive_db');
 ?>
